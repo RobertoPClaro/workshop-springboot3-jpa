bd7d9ed1e3a50f359e8f8c04cf9d31b6c4e336f4
Roberto Pacheco Claro <robertopacheco1994@gmail.com>
2023-04-04 08:44:53 -0300

    User entity and resource

---
+++ src/main/java/com/example/curse/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.example.curse.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.curse.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User user = new User(1L, "Maria", "mariga@gmail.com", "11993231418", "123456");
+		return ResponseEntity.ok().body(user);
+	}
+	
+}
